4553bf3216c1c7a1d9c2621769c58c0c45cbd55f
ourgithubacc <111426399+ourgithubacc@users.noreply.github.com>
2023-01-08 03:35:28 +0100

    Update sendEmail.js

--- helper/sendEmail.js
+++ helper/sendEmail.js
@@ -1,71 +1,35 @@
-const nodemailer = require("nodemailer");
-require('dotenv').config()
+const nodemailer = require('nodemailer')
 
-
- function sendEmail(email, message, subject) {
-  
-    const transport = nodemailer.createTransport({
-      host:"smtp.mailtrap.io",
+exports.sendEmail = async (email,body,subject) =>{
+  try {
+    let transporter = nodemailer.createTransport({
+      host: 'smtp.mailtrap.io',
       port:2525,
-      secure: false,
       auth: {
-        user: '0c3804aa601ee5',
-        pass:'b1e5ef36b5ef91'
+        user: '2a47fd6770a84d',
+        pass: '248e23590fa3eb'
       }
     });
     
-
-  return transport.sendMail({
-    from: 'BUSA', // sender address
-    to: email, // list of receivers
-    subject, // Subject line
-    text:'test', // plain text body
-    html: message, // html body
-    // attachments:[
-    //   {
-    //     filename:'QRcode.png',
-    //     path:'./QRcode.png'
-    //   }
-    // ]
-  });
-
-
-}
-
-
-
-
- function sendQr(email,message,subject) {
-  
-    const transport = nodemailer.createTransport({
-      host:"smtp.mailtrap.io",
-      port:2525,
-      secure: false,
-      auth: {
-        user: '0c3804aa601ee5',
-        pass:'b1e5ef36b5ef91'
+    let mailOptions = {
+      from: 'BUSA',
+      to: email,
+      subject,
+      text:body
+    };
+    
+    transporter.sendMail(mailOptions, function(err, data) {
+      if (err) {
+        console.log("Error " + err);
+      } else {
+        console.log("Email sent successfully");
       }
     });
     
-
-  return  transport.sendMail({
-    from: '"Busa" <foo@example.com>', // sender address
-    to: email, // list of receivers
-    subject, // Subject line
-    text:'test', // plain text body
-    html:message, // html body
-    attachments:[
-      {
-        filename:'QRcode.png',
-        path:'./QRcode.png'
-      }
-    ]
-  });
-
- 
-  
+    
+  } catch (error) {
+    
+  }
 }
 
 
-
-  module.exports = {sendEmail, sendQr};
